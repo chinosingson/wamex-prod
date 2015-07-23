5597443f36b0a7fd35d705dfbc036d81fbb7eefa
Chino Singson <chinosingson@gmail.com>
2015-07-23 11:19:20 +0800

    Updates 2015-07-22 (2) - Bootstrap WaMEx subtheme

---
+++ sites/all/themes/bootstrap/wamex/template.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * template.php
+ */
+
+ 
+function wamex_theme($variables) {
+	
+	return array(
+		'project_node_form' => array(
+			'arguments' => array('form' => NULL),
+			'render element' => 'form',
+			//'path' => base_path().drupal_get_path('theme','gms').'/templates',
+			'path' => drupal_get_path('theme','wamex').'/templates',
+			'template' => 'node--project--edit'),
+	);
+}
+
+
+function wamex_form_alter(&$form, &$form_state, $form_id) {
+	if ($form_id == 'project_node_form'){
+		$form['title']['#title'] = t('Project Name');
+		$form['body'][LANGUAGE_NONE][0]['#title'] = t('Description');
+		$form['body'][LANGUAGE_NONE][0]['#format'] = 'plain_text';
+		//$form['field_population'][LANGUAGE_NONE][0]['#attributes']['type'] = 'number';
+		//$form['field_author'][LANGUAGE_NONE][0]['#title'] = t('Created by');
+		
+		hide($form['body'][LANGUAGE_NONE][0]['summary']);
+		//hide($form['body'][LANGUAGE_NONE][0]['format']);
+		//$form['body'][LANGUAGE_NONE][0]['format']['#access'] = FALSE;
+	}
+	return $form;
+}
---
+++ sites/all/themes/bootstrap/wamex/templates/node--project--edit.tpl.php
@@ -0,0 +1,22 @@
+﻿<?php 
+	$node = menu_get_object(); 
+	@$nid = $node->nid;
+
+	//echo "<pre style='height: 500px; overflow-y: scroll'>".print_r($form['field_author'],1)."</pre>";
+
+?>
+
+	<div id="project-forms-container" class="form-container container-fluid">
+		<?php print drupal_render($form['title']); ?>
+		<?php print drupal_render($form['field_location']); ?>
+		<?php print drupal_render($form['field_population']); ?>
+		<?php print drupal_render($form['body']); ?>
+		<?php print drupal_render($form['field_author']); ?>
+		<?php print drupal_render($form['field_discount_rate']); ?>
+		<?php print drupal_render($form['field_ci_cost']); ?>
+		<?php print drupal_render($form['field_currency']); ?>
+		<?php print drupal_render($form['field_exchange_rate_to_usd']); ?>
+		<div id="project-forms-buttons" class="row"><?php print drupal_render_children($form); ?></div>
+	</div>
+
+
