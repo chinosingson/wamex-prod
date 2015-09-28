2f7f39979d3ac4db6d0d4c81e28eec70c6d445e6
Chino Singson <chinosingson@gmail.com>
2015-09-28 17:20:34 +0800

    Updates 2015-09-28 (2)

---
+++ sites/all/themes/bootstrap/wamex/templates/views-view-table--loading--block.tpl.php
@@ -0,0 +1,70 @@
+﻿<?php
+$node = menu_get_object(); 
+
+/**
+ * @file
+ * Template to display a view as a table.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $header: An array of header labels keyed by field id.
+ * - $caption: The caption for this table. May be empty.
+ * - $header_classes: An array of header classes keyed by field id.
+ * - $fields: An array of CSS IDs to use for each field id.
+ * - $classes: A class or classes to apply to the table, based on settings.
+ * - $row_classes: An array of classes to apply to each row, indexed by row
+ *   number. This matches the index in $rows.
+ * - $rows: An array of row items. Each row is an array of content.
+ *   $rows are keyed by row number, fields within rows are keyed by field ID.
+ * - $field_classes: An array of classes to apply to each field, indexed by
+ *   field id, then row number. This matches the index in $rows.
+ * @ingroup views_templates
+ */
+?>
+<table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?> id="view-project-loadings">
+   <?php if (!empty($title) || !empty($caption)) : ?>
+     <caption><?php print $caption . $title; ?></caption>
+  <?php endif; ?>
+  <?php if (!empty($header)) : ?>
+    <thead>
+      <tr>
+        <?php foreach ($header as $field => $label): ?>
+          <th <?php if ($header_classes[$field]) { print 'class="'. $header_classes[$field] . '" '; } ?>>
+            <?php print $label; ?>
+          </th>
+        <?php endforeach; ?>
+      </tr>
+    </thead>
+  <?php endif; ?>
+  <tbody>
+    <?php foreach ($rows as $row_count => $row): 
+			$row_classes[] = 'view-field-display';
+		?>
+      <tr id="loading-<?php print $row['nid']; ?>" <?php if ($row_classes[$row_count]) { print 'class="' . implode(' ', $row_classes[$row_count]) .'"';  } ?>>
+        <?php foreach ($row as $field => $content): ?>
+          <td <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+            <?php print $content; ?>
+          </td>
+        <?php endforeach; ?>
+      </tr>
+    <?php endforeach; ?>
+		<!--tr id="loading-form-row">
+			<td colspan="12" id="loading-form-container">
+			</td>
+		</tr-->
+  </tbody>
+	<tfoot>
+		<tr id="#average-loadings">
+			<td>&nbsp;</td>
+			<td>Average Loadings</td>
+			<td class="average-loading" id="ave_adwf"></td>
+			<td class="average-loading" id="ave_cod"></td>
+			<td class="average-loading" id="ave_bod5"></td>
+			<td class="average-loading" id="ave_totn"></td>
+			<td class="average-loading" id="ave_totp"></td>
+			<td class="average-loading" id="ave_tss"></td>
+			<td class="average-loading" id="tot_weight"></td>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+		</tr>
+	</tfoot>
+</table>
